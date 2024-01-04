71f5e9a8e9f42f0b0a26350dc75adb5a20e98c57
plantshley <Ashley.Geraets@colostate.edu>
2024-01-04 11:30:00 -0600

    new paths

--- CDPHE_app_v1/ui.R
+++ CDPHE_app_v1/ui.R
@@ -1,15 +1,16 @@
-source(paste0(rstudioapi::getActiveProject(),"/ui/app_funs.R"))
+source(paste0(rstudioapi::getActiveProject(),"/CDPHE_app_v1/ui/app_funs.R"))
 
 ### UI Start ###
 
 fluidPage(
   
-  includeCSS(paste0(getActiveProject(), "/www/css_cdphe.css")),
-
+  includeCSS(paste0(getActiveProject(), "/CDPHE_app_v1/www/css_cdphe.css")),
   #tags$head(
-    #tags$link(rel = "stylesheet", type = "text/css", href = paste0(getwd(), "/www/css_cdphe.css"))),
+    #tags$link(rel = "stylesheet", type = "text/css", href = paste0(getwd(), "/CDPHE_app_v1/www/css_cdphe.css"))),
 
   waiter::useWaiter(), 
+  shinyjs::useShinyjs(),
+
   # Dynamic Title
   uiOutput("title"), 
   
@@ -31,10 +32,10 @@ fluidPage(
             
     tabsetPanel(id = "tabs", selected = "home_tab",
                 
-      source(paste0(getActiveProject(), "/ui/tab_home.R"))$value,
-      source(paste0(getActiveProject(), "/ui/tab_univ.R"))$value,
-      source(paste0(getActiveProject(), "/ui/tab_mv.R"))$value,
-      source(paste0(getActiveProject(), "/ui/tab_gloss.R"))$value
+      source(paste0(getActiveProject(), "/CDPHE_app_v1/ui/tab_home.R"))$value,
+      source(paste0(getActiveProject(), "/CDPHE_app_v1/ui/tab_univ.R"))$value,
+      source(paste0(getActiveProject(), "/CDPHE_app_v1/ui/tab_mv.R"))$value,
+      source(paste0(getActiveProject(), "/CDPHE_app_v1/ui/tab_gloss.R"))$value
       )
     )
   )
